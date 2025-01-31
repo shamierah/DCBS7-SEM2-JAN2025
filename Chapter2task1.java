20ad1733c5f53125a9c1a17153dfd53236596eed
afeefmizra <afeef0808@gmail.com>
2025-01-31 12:19:38 +0800

    Add files via upload

--- Chapter2task1.java
+++ Chapter2task1.java
@@ -1,39 +1,23 @@
 import java.util.Scanner;
 
 public class ArithmeticProg {
-    public static void main(String[] args) {
-        // Create a Scanner object for input
+public static void main(String[] args) {
+	
+	int num1, num2;
+	
+
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Please masukkan number first anda: ");
+        num1 = scanner.nextInt();
 
-        // Ask the user to input two integers
-        System.out.print("Enter the first integer: ");
-        int num1 = scanner.nextInt();
+        System.out.print("Okay pastu letak la lagi satu nombor: ");
+        num2 = scanner.nextInt();
 
-        System.out.print("Enter the second integer: ");
-        int num2 = scanner.nextInt();
-
-        // Perform arithmetic operations
-        int sum = num1 + num2;
-        int difference = num1 - num2;
-        int product = num1 * num2;
-        int quotient = (num2 != 0) ? num1 / num2 : 0; // Avoid division by zero
-        int remainder = (num2 != 0) ? num1 % num2 : 0; // Avoid division by zero
-
-        // Display the results
-        System.out.println("\nArithmetic Operations:");
-        System.out.println("Sum: " + sum);
-        System.out.println("Difference: " + difference);
-        System.out.println("Product: " + product);
-        if (num2 != 0) {
-            System.out.println("Quotient: " + quotient);
-            System.out.println("Remainder: " + remainder);
-        } else {
-            System.out.println("Division and modulus operations cannot be performed as the second number is zero.");
-        }
-
-        // Close the scanner
-        scanner.close();
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+      
     }
 }
-
-
