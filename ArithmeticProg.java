c4b29282fd7d7c3cc9751bb6f223cbb6c49a3c08
shamierah <shamierahrasheedah@gmail.com>
2025-02-06 20:23:45 +0800

    Add files via upload

--- ArithmeticProg.java
+++ ArithmeticProg.java
@@ -1,21 +1,42 @@
-import java.util.Scanner;
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arithmeticprog;
+import java.util.*; //library
+/**
+ *
+ * @author sammy
+ */
 public class ArithmeticProg {
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        int num1, num2;
-        Scanner in = new Scanner(System.in);
+    int num1, num2;
         
-        System.out.print("Enter your first number : ");
-        num1 = in.nextInt();
+  
         
-        System.out.print("Enter your second number : ");
-        num2 = in.nextInt();
-        
-        System.out.println("\nThis is the answer for all arithmetic equation :-");
-        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
-        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
-        System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
-        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
-        System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
-    }
+    Scanner read = new Scanner(System.in); //read input
+    
+    System.out.println("PLEASE ENTER FIRST NUMBER : ");
+    num1 = read.nextInt();
+    System.out.println("PLEASE ENTER SECOND NUMBER : ");
+    num2 = read.nextInt();
+    
+    read.close();
+    
+    System.out.println("CALCULATION"); //operation
+    int add = num1 + num2;
+    System.out.println("addition    : " +num1 +"+" +num2 +"=" +add);
+    int sub = num1 - num2;
+    System.out.println("subtraction : " +num1 +"-" +num2 +"=" +sub);
+    int mul = num1 * num2;
+    System.out.println("multiply    : " +num1 +"X" +num2 +"=" +mul);
+    int div = num1 / num2;
+    System.out.println("division    : " +num1 +"/" +num2 +"=" +div);
+    int mod = num1 % num2;
+    System.out.println("modulus     : " +num1 +"%" +num2 +"=" +mod);
+}
+    
 }
